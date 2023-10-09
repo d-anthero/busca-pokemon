640783c87c1666e1460bd5b457af85c163dcea80
d-anthero <d.anthero09@gmail.com>
2023-10-09 12:08:57 -0300

    busca-pokemon start
    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div class="container">
+  <app-card></app-card>
+  </div>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
